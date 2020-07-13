7f378c401883cd1ad6cc3d2ac28c74b90b3f04b4
Tony Mackay <hello@tonymackay.net>
2020-07-13 11:12:11 +0100

    feat: Implement hash and verify funcs

--- hasher.go
+++ hasher.go
@@ -1,8 +1,15 @@
 package main
 
-import "errors"
+import (
+	"golang.org/x/crypto/bcrypt"
+)
 
-func hash(password string, cost int) (hash string, err error) {
-	err = errors.New("hash not implemented")
-	return hash, err
+func hash(password string, cost int) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), cost)
+	return string(bytes), err
+}
+
+func verify(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
--- main.go
+++ main.go
@@ -6,9 +6,11 @@ import (
 	"os"
 )
 
+const defaultCost = 10
+
 func main() {
 	password := flag.String("password", "", "Password to hash")
-	cost := flag.Int("cost", 10, "Cost between 4 and 31")
+	cost := flag.Int("cost", defaultCost, "Cost between 4 and 31")
 
 	flag.Usage = func() {
 		fmt.Printf("\nUsage: go-hash [OPTIONS]\n\n")
@@ -21,17 +23,36 @@ func main() {
 
 	flag.Parse()
 
+	// make sure a password was provided
 	if *password == "" {
-		fmt.Printf("error: %v\n", "a password must be provided with the -password flag")
+		fmt.Printf("error: a password must be provided with the -password flag\n")
 		os.Exit(2)
 	}
 
-	fmt.Printf("hashing: %v\n", *password)
+	// validate -cost flag meets allowed range
+	if *cost < 4 || *cost > 31 {
+		fmt.Printf("error: cost %v is outside allowed range (4,31)\n", *cost)
+		os.Exit(2)
+	}
 
+	// hash the password
 	hash, err := hash(*password, *cost)
 	if err != nil {
 		fmt.Printf("error: %v\n", err)
 		os.Exit(2)
 	}
-	fmt.Println(hash)
+
+	// double check generated hash can be compared with original password
+	if !verify(*password, hash) {
+		fmt.Printf("error: password failed verification\n")
+		os.Exit(2)
+	}
+
+	// display the hashed password
+	fmt.Printf("hash: %v\n", hash)
+
+	// output cost used when -cost flag not specified
+	if *cost == defaultCost {
+		fmt.Printf("cost: %v\n", *cost)
+	}
 }
